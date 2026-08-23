3e98ae4fda57cce767652bafd089b1b918bf4a0a
MayankSingh-2002 <mksnavodaya@gmail.com>
2026-08-23 14:18:08 +0530

    Java Coding for printing numbers

---
+++ Java/IdeaProjects/Num_Print/src/Num_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Num_Print {
+    static void main() {
+        int number = 18;
+        System.out.println("The number is: " + number);
+        System.out.println(2026);
+    }
+}
