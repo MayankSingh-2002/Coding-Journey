c825d171d09b6b7617d51dc7b60d531ad0dc581a
MayankSingh-2002 <mksnavodaya@gmail.com>
2026-08-25 21:41:44 +0530

    Java Coding for reverse half right pyramid pattern.

---
+++ Java/IdeaProjects/Day 7 Patterns/src/ReverseRightHalfPyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ReverseRightHalfPyramid {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no. of rows in reverse pyramid: ");
+
+        int n = sc.nextInt();
+
+        for (int i = n ; i >= 1; i--) {
+            for (int j = i; j >= 1; j-- )
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
