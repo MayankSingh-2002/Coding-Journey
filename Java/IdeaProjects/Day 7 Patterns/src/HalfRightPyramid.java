8ad51af53129c1d345ab0319d05fc390697a9cdc
MayankSingh-2002 <mksnavodaya@gmail.com>
2026-08-25 21:35:03 +0530

    Java Coding for half right pyramid pattern.

--- Java/IdeaProjects/Day 7 Patterns/src/HalfRightPyramid.java
+++ Java/IdeaProjects/Day 7 Patterns/src/HalfRightPyramid.java
@@ -1,15 +1,13 @@
-//16
 import java.util.*;
 
-public class SquarePattern {
+public class HalfRightPyramid {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Enter the no. of rows in the pattern: ");
         int n = sc.nextInt();
 
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <n; j++)
+        for (int i = 1; i <=n; i++){
+            for (int j = 1; j <=i; j++)
                 System.out.print("*");
             System.out.println();
         }
